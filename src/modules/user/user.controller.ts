28fd98d38aac4816aac34e466408396b9db81b85
Arthur Charly <tulokitto1998@gmail.com>
2021-01-13 18:54:46 -0500

    [ADD]: role decorator and role guard.

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,5 +1,7 @@
 import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { Roles } from '../role/decorators/role.decorator';
+import { RoleGuard } from '../role/guards/role.guard';
 import { User } from './user.entity';
 import { UserService } from './user.service';
 
@@ -8,8 +10,9 @@ export class UserController {
     constructor(private readonly _userService:UserService) {
         
     }
-
     @Get(':id')
+    @Roles('ADMIN')
+    @UseGuards(AuthGuard(),RoleGuard)
     async getUser(@Param('id',ParseIntPipe) id:number):Promise<User>{
         const user = this._userService.get(id);
         return user;
